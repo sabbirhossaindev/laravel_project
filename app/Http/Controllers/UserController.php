573de0aa29da6f8b70041e0f89b87961dd596868
sabbir hossain <mdsabbir477470@gmail.com>
2023-05-16 11:38:07 +0600

    done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,52 +7,94 @@ use App\User;
 
 class UserController extends Controller
 {
-    // create a user.
-    public function create(){
-        $data = [
-            'name' => 'Sabbir Hossain',
-            'email' => 'mdsabbir477470@gmail.com',
-            'password' => 'sabbir50'
-        ];
+    // // create a user.
+    // public function create(){
+    //     $data = [
+    //         'name' => 'Sabbir Hossain',
+    //         'email' => 'mdsabbir477470@gmail.com',
+    //         'password' => 'sabbir50'
+    //     ];
 
-        User::create($data);
-        return ('User create Successfully.');
+    //     User::create($data);
+    //     return ('User create Successfully.');
+    // }
+
+    // // delete a user
+    // public function delete($id){
+    //     {
+    //         $user = User::fiendOrFail($id);
+    //         $user->delete();
+
+    //         return 'Deleted';
+    //     }
+    // }
+
+    // // delete a user
+    // public function edit($id){
+    //     {
+    //         $user = User::fiendOrFail($id);
+    //         $user->name = 'New name';
+    //         $user->save();
+
+    //         return 'Edit Done!';
+    //     }
+    // }
+
+    // // database thka user dackta
+    // public function index(){
+    //     {
+    //         $users = User::all();
+    //         return $users;
+    //     }
+    // }
+
+    // // single data show
+    // public function show($id){
+    //     {
+    //         $user = User::find($id);
+    //         return $user;
+    //     }
+    // }
+    public function index()
+	{
+		$users = User::all();
+
+		return $users;
+	}
+
+    public function show($id, $email = 'mamun@gmail.com')
+    {
+        return $id . ' ' . $email;
     }
 
-    // delete a user
-    public function delete($id){
-        {
-            $user = User::fiendOrFail($id);
-            $user->delete();
-
-            return 'Deleted';
-        }
+    public function display($id)
+    {
+        return 'Display ' . $id;
     }
 
-    // delete a user
-    public function edit($id){
-        {
-            $user = User::fiendOrFail($id);
-            $user->name = 'New name';
-            $user->save();
-
-            return 'Edit Done!';
-        }
+    public function create()
+    {
+        return view('create_user');
     }
 
-    // database thka user dackta
-    public function index(){
-        {
-            $users = User::all();
-            return $users;
-        }
+    public function store()
+    {
+        return 'Added a user';
     }
 
-    // single data show
-    public function show($id){
-        {
-            $user = User::find($id);
-            return $user;
-        }
+    public function update()
+    {
+        return 'Updated';
+    }
+
+    public function updateName()
+    {
+        return 'Name has been updated';
+    }
+
+
+    public function delete()
+    {
+        return 'Deleted';
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,32 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('home', [HomeController::class, 'payment']);
-Route::get('contact', [ContactController::class, 'index']);
-Route::get('user', [UserController::class, 'create']);
+// Route::get('home', [HomeController::class, 'payment']);
+// Route::get('contact', [ContactController::class, 'index']);
+// Route::get('user', [UserController::class, 'create']);
+
+// Route::get('users', 'UserController@index');
+// Route::get('create_user', 'UserController@create');
+// Route::post('add-user', 'UserController@store');
+// Route::put('update-user', 'UserController@update');
+// Route::patch('update-user-name', 'UserController@updateName');
+// Route::delete('delete-user', 'UserController@delete');
+
+Route::get('user-list', 		'UserController@index')->name('user_list');
+Route::get('users/{id}/profile/{email?}', 		'UserController@show');
+
+
+
+Route::get('users/{id}', 	'UserController@display')->where('id', '[0-9]+');
+Route::get('users/create',  'UserController@create');
+
+Route::post('users', 		'UserController@store');
+Route::put('users', 		'UserController@update');
+Route::patch('users', 		'UserController@updateName');
+Route::delete('users', 		'UserController@delete');
+
+Route::redirect('/test', '/test-any');
+
+Route::any('test-any', function () {
+    return 'I am from Any';
+});
