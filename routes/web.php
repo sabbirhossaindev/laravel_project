a0ff4df9613129422d36c82a13d64e2f3034986c
sabbir hossain <mdsabbir477470@gmail.com>
2023-07-05 12:05:39 +0600

    add done

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,8 @@ Route::get('/me/{name}', function ($userName) {
     return view("welcome {$userName}");
 });
 
+// user tha ke input;
+
 
 Route::get('home', [HomeController::class, 'payment']);
 Route::get('contact', [ContactController::class, 'index']);
